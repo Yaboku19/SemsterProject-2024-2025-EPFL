1289e28da76ae9b20ce0a8ad4d2b240bb75fa90f
Yaboku19 <emanuele.martelli19@gmail.com>
2025-02-21 16:58:53 +0100

    first commit

---
+++ sumOfBigNumber.c
@@ -0,0 +1,286 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <immintrin.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <time.h>
+#include <stdint.h>
+#include <gmp.h>
+
+typedef struct {
+    uint64_t low;
+    uint64_t high;
+} uint128_t;
+
+void generate_large_number(uint64_t* num, int length) {
+    for (int i = 0; i < length; i++) {
+        num[i] = UINT64_MAX;
+    }
+}
+
+uint128_t traditional_sum(uint64_t* num, int length) {
+    uint128_t result = {0, 0};
+    for (int i = 0; i < length; i++) {
+        uint64_t new_low = result.low + num[i];
+        if (new_low < result.low) {
+            result.high++;
+        }
+        result.low = new_low;
+    }
+    return result;
+}
+
+uint128_t simd_sum(uint64_t* num, int length) {
+    __m256i sum = _mm256_setzero_si256();
+    uint128_t result = {0, 0};
+    uint64_t overflow_count = 0;
+    uint64_t oldSum[4];
+
+    for (int i = 0; i < length; i += 4) {
+        __m256i data = _mm256_loadu_si256((__m256i*)(num + i));
+        for (int j = 0; j < 4; j++) {
+            oldSum[j] = ((uint64_t*)&sum)[j];
+        }
+        sum = _mm256_add_epi64(sum, data);
+
+        for (int j = 0; j < 4; j++) {
+            uint64_t old_val = oldSum[j];
+            uint64_t new_val = ((uint64_t*)&sum)[j];
+            if (new_val >= old_val) {
+                continue;
+            }
+            overflow_count++;
+            if (j < 3) {
+                ((uint64_t*)&sum)[j + 1] -= 1;
+            }
+
+        }
+    }
+    printf("over: %lu\n", overflow_count);
+    uint64_t carry = 0;
+    for (int j = 0; j < 4; j++) {
+        uint64_t temp = result.low + ((uint64_t*)&sum)[j];
+        if (temp < result.low) {
+            carry++;
+        }
+        result.low = temp;
+    }
+    result.high = overflow_count + carry;
+    return result;
+}
+
+uint128_t simd_sum_v2(uint64_t* num, int length) {
+    uint64_t result_array[4] = {0, 0, 0, 0};
+    uint128_t result;
+    uint64_t overflow_count = 0;
+    uint64_t *oldSum = malloc(4 * sizeof(uint64_t));
+    uint64_t *currentSum = malloc(4 * sizeof(uint64_t));
+    const char *fmt = "valore: %lu\n";
+    if(oldSum == NULL || currentSum == NULL) {
+        printf("Memory allocation failed\n");
+        exit(1);
+    }
+    asm volatile(
+        "vpxor %%ymm0, %%ymm0, %%ymm0\n"                // set ymm0 to zero
+        "1:\n"
+        "   vpxor %%ymm1, %%ymm1, %%ymm1\n"  
+        "   vmovdqu (%[nums]), %%ymm1\n"                 // load 256 bits from num into ymm1
+        "   vmovdqu %%ymm0, (%3)\n"                     // store current ymm0 to oldSum before summing
+        "   vpaddq %%ymm1, %%ymm0, %%ymm0\n"            // add ymm1 to ymm0, correct to use 64-bit add
+        "   vmovdqu %%ymm0, (%4)\n"                     // store current ymm0 to currentSum after summing
+        "   add $32, %0\n"                              // move pointer to the next 256 bits
+        "   sub $4, %[len]\n"                               // decrement length by 4
+        "   mov $4, %%ecx\n"                            // Set loop counter for element comparison (4 elements, index 3 to 0)
+        "   mov %3, %%rsi\n"                           // Save oldSum in rsi
+        "   mov %4, %%rdi\n"                           // Save currentSum in rdi
+        "2:\n"
+        "   movq (%%rsi), %%rax\n"                          // Load from oldSum for comparison
+        "   movq (%%rdi), %%rdx\n"                          // Load from currentSum for comparison
+        "   cmp %%rax, %%rdx\n"                             // Compare currentSum[i] with oldSum[i]
+        "   jge 3f\n"                                       // Jump if currentSum[i] >= oldSum[i], skip to next
+        "   incq %5\n"                                      // Increment overflow_count
+        "3:\n"
+        "   lea 8(%%rdi), %%rdi\n"                      // Move currentSum pointer
+        "   lea 8(%%rsi), %%rsi\n"                      // Move oldSum pointer
+        "   loop 2b\n"                                  // Decrement ecx and loop back to 2:
+        "   jg 1b\n"                                    // loop until length is not greater than 0
+        "vmovdqu %%ymm0, %2\n"                          // store result from ymm0 to result_array
+        : [nums]"+r" (num), [len] "+r" (length), "+m" (result_array), "+r" (oldSum), "+r" (currentSum), "+m" (overflow_count)
+        :
+        : "ymm0", "ymm1", "rcx", "rax", "rdx", "rsi", "rdi", "cc", "memory"
+    );
+    result = traditional_sum(result_array, 4);
+    result.high += overflow_count;
+    printf("over: %lu\n", overflow_count);
+    free(oldSum);
+    free(currentSum);
+    return result;
+}
+
+uint128_t simd_sum_32(int length, uint64_t* low, uint64_t* high) {
+    uint128_t result = {0, 0};
+    // Divisione in parte alta e bassa
+
+    // Somma SIMD delle parti basse
+    __m256i sum_low = _mm256_setzero_si256();
+    __m256i sum_high = _mm256_setzero_si256();
+    for (int i = 0; i < length; i += 4) {
+        __m256i data = _mm256_load_si256((__m256i*)(low + i));
+        __m256i data2 = _mm256_load_si256((__m256i*)(high + i));
+        sum_low = _mm256_add_epi64(sum_low, data);
+        sum_high = _mm256_add_epi64(sum_high, data2);
+    }
+
+    uint64_t highValue = 0;
+    uint64_t lowValue = 0;
+    for (int j = 0; j < 4; j++) {
+        highValue += ((uint64_t*)&sum_high)[j];
+        lowValue += ((uint64_t*)&sum_low)[j];
+    }
+    __uint128_t total = ((__uint128_t)(highValue + (lowValue >> 32)) << 32) | lowValue & 0xFFFFFFFF;
+    // // Assegna i primi 64 bit a result.low
+    result.low = (uint64_t)(total & 0xFFFFFFFFFFFFFFFF);
+
+    // // Assegna i bit rimanenti a result.high
+    result.high = (uint64_t)(total >> 64);
+    return result;
+}
+
+uint128_t simd_sum_32_ass(int length, uint64_t* low, uint64_t* high) {
+    uint128_t result = {0, 0};
+    uint64_t lowValue, highvalue;
+    asm volatile (
+        "vpxor %%ymm0, %%ymm0, %%ymm0\n"
+        "vpxor %%ymm1, %%ymm1, %%ymm1\n"
+        "1:\n"
+        "   vpxor %%ymm2, %%ymm2, %%ymm2\n"
+        "   vmovdqu (%[low]), %%ymm2\n"  
+        "   vpaddq %%ymm2, %%ymm0, %%ymm0\n"
+        "   vpxor %%ymm2, %%ymm2, %%ymm2\n"
+        "   vmovdqu (%[high]), %%ymm2\n"  
+        "   vpaddq %%ymm2, %%ymm1, %%ymm1\n"
+        "   add $32, %[low]\n"
+        "   add $32, %[high]\n"
+        "   sub $4, %[len]\n"
+        "   jg 1b\n"
+        "xor %%rax, %%rax\n"
+        "vextracti128 $1, %%ymm0, %%xmm0\n"
+        "pextrq $1, %%xmm0, %%rcx\n"
+        "addq %%rcx, %%rax\n"
+        "pextrq $0, %%xmm0, %%rcx\n"
+        "addq %%rcx, %%rax\n"
+        "vextracti128 $0, %%ymm0, %%xmm0\n"
+        "pextrq $1, %%xmm0, %%rcx\n"
+        "addq %%rcx, %%rax\n"
+        "pextrq $0, %%xmm0, %%rcx\n"
+        "addq %%rcx, %%rax\n"
+
+        "xor %%rbx, %%rbx\n"
+        "vextracti128 $1, %%ymm1, %%xmm0\n"
+        "pextrq $1, %%xmm0, %%rcx\n"
+        "addq %%rcx, %%rbx\n"
+        "pextrq $0, %%xmm0, %%rcx\n"
+        "addq %%rcx, %%rbx\n"
+        "vextracti128 $0, %%ymm1, %%xmm0\n"
+        "pextrq $1, %%xmm0, %%rcx\n"
+        "addq %%rcx, %%rbx\n"
+        "pextrq $0, %%xmm0, %%rcx\n"
+        "addq %%rcx, %%rbx\n"
+        
+        "movq %%rax, %[lowVal]\n"
+        "movq %%rbx, %[highVal]\n"
+        : [low]"+r" (low), [high]"+r" (high), [len] "+r" (length), [lowVal] "+m" (lowValue), [highVal] "+m" (highvalue) 
+        :
+        : "ymm0", "ymm1", "ymm2", "rax", "rbx", "rcx", "xmm0", "memory"
+    );
+    __uint128_t total = ((__uint128_t)(highvalue + (lowValue >> 32)) << 32) | lowValue & 0xFFFFFFFF;
+    result.low = (uint64_t)(total & 0xFFFFFFFFFFFFFFFF);
+    result.high = (uint64_t)(total >> 64);
+    return result;
+}
+
+void print_uint128(uint128_t num) {
+    mpz_t big_num;
+    mpz_init(big_num);
+    mpz_set_ui(big_num, num.high);  // Imposta la parte alta.
+    mpz_mul_2exp(big_num, big_num, 64);  // Sposta la parte alta di 64 bit a sinistra.
+    mpz_add_ui(big_num, big_num, num.low);  // Aggiungi la parte bassa.
+
+    char *text = mpz_get_str(NULL, 10, big_num);  // Converti il numero a stringa in base 10.
+    printf("%s\n", text);  // Stampa il numero.
+    printf("up: %lx down: %lx\n", num.high, num.low);
+    free(text);  // Libera la memoria allocata da GMP.
+
+    mpz_clear(big_num);  // Pulisci la memoria usata da GMP.
+}
+
+int main(int argc, char* argv[]) {
+    int size;
+    if (argc < 2) {
+        size = 20000;
+    } else {
+        size = atoi(argv[1]);
+    }
+    uint64_t *num = malloc(size * 4 * sizeof(uint64_t));
+    if (num == NULL) {
+        printf("Memory allocation failed\n");
+        return 1;
+    }
+    generate_large_number(num, size*4);
+
+    clock_t start = clock();
+    uint128_t result = traditional_sum(num, size*4);
+    clock_t end = clock();
+    double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("Time spent in traditional sum: %.3f seconds, Result: ", time_spent);
+    print_uint128(result);
+
+    start = clock();
+    result = simd_sum(num, size*4);
+    end = clock();
+    time_spent = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("Time spent in SIMD sum: %.3f seconds, Result: ", time_spent);
+    print_uint128(result);
+    
+    uint64_t *low, *high;
+    // Usa posix_memalign per evitare problemi di allineamento
+    if (posix_memalign((void**)&low, 64, size*4 * sizeof(uint64_t)) != 0 ||
+        posix_memalign((void**)&high, 64, size*4 * sizeof(uint64_t)) != 0) {
+        printf("Memory allocation failed\n");
+        exit(1);
+    }
+    for (int i = 0; i < size*4; i++) {
+        low[i] = num[i] & 0xFFFFFFFF;       // Parte bassa
+        high[i] = (num[i] >> 32) & 0xFFFFFFFF; // Parte alta
+    }
+
+    start = clock();
+    result = simd_sum_32(size*4, low, high);
+    end = clock();
+    time_spent = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("Time spent in SIMD_v4 sum: %.3f seconds, Result: ", time_spent);
+    print_uint128(result);
+
+    start = clock();
+    result = simd_sum_32_ass(size*4, low, high);
+    end = clock();
+    time_spent = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("Time spent in SIMD_v4 sum: %.3f seconds, Result: ", time_spent);
+    print_uint128(result);
+    free(low);
+    free(high);
+    free(num);
+
+    return 0;
+}
+
+/*
+    Compile: gcc -g -o sumOfBigNumber sumOfBigNumber.c -mavx2 -O2 -lgmp
+    ./sumOfBigNumber 400000000
+    Time spent in traditional sum: 0.265 seconds, Result: 18446744073309551616
+    Time spent in SIMD sum: 0.202 seconds, Result: 18446744073309551616
+    Time spent in SIMD_v2 sum: 0.207 seconds, Result: 18446744073309551616
+    Time spent in SIMD_v3 sum: 0.201 seconds, Result: 16728757172089420800
+    UINT64_MAX
+*/
+
